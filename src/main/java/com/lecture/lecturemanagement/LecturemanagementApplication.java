bcb4a6c1fac085d440fe646514134bfa3e229605
wwlee94 <38131683+wwlee94@users.noreply.github.com>
2019-05-15 13:18:42 +0900

    # First Commit

---
+++ src/main/java/com/lecture/lecturemanagement/LecturemanagementApplication.java
@@ -0,0 +1,13 @@
+package com.lecture.lecturemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LecturemanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LecturemanagementApplication.class, args);
+    }
+
+}
