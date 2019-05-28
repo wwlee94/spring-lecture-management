bd8cd565428042e66beb52b1787816847b8f6f0f
wwlee94 <38131683+wwlee94@users.noreply.github.com>
2019-05-28 17:52:09 +0900

    # bootstrap 적용

--- src/main/java/com/lecture/lecturemanagement/HomeController.java
+++ src/main/java/com/lecture/lecturemanagement/HomeController.java
@@ -1,13 +1,13 @@
 package com.lecture.lecturemanagement;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
     public String root(){
-        return "Woowon";
+        return "index";
     }
 }
