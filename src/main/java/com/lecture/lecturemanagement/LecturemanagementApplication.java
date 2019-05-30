8386e1b733e3a24f7c566980813fb352020bed5c
tjdds1109 <tjdds1109@naver.com>
2019-05-30 23:55:29 +0900

    일단 커밋

--- src/main/java/com/lecture/lecturemanagement/LecturemanagementApplication.java
+++ src/main/java/com/lecture/lecturemanagement/LecturemanagementApplication.java
@@ -1,9 +1,12 @@
 package com.lecture.lecturemanagement;
 
+import com.lecture.lecturemanagement.member.MemberRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages={"com.lecture.lecturemanagement"})
+@EnableJpaRepositories(basePackageClasses = MemberRepository.class)
 public class LecturemanagementApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/lecture/lecturemanagement/member/MemberRepository.java
+++ src/main/java/com/lecture/lecturemanagement/member/MemberRepository.java
@@ -1,7 +1,9 @@
 package com.lecture.lecturemanagement.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Member findByUemail(String email);
