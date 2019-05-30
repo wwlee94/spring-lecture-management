f750a0278ec91c450ecb9356bd92e49522aef44d
tjdds1109 <tjdds1109@naver.com>
2019-05-31 00:28:10 +0900

    일단 커밋

--- src/main/java/com/lecture/lecturemanagement/LecturemanagementApplication.java
+++ src/main/java/com/lecture/lecturemanagement/LecturemanagementApplication.java
@@ -1,12 +1,9 @@
 package com.lecture.lecturemanagement;
 
-import com.lecture.lecturemanagement.member.MemberRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages={"com.lecture.lecturemanagement"})
-@EnableJpaRepositories(basePackageClasses = MemberRepository.class)
+@SpringBootApplication
 public class LecturemanagementApplication {
 
     public static void main(String[] args) {
